c4a2316d0d3fc73bdf289c16b01429b8edbfeec7
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2022-12-28 12:59:22 +0530

    Added new conditionals.sh file

---
+++ Conditionals.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ 8 -gt 7 ];
+then 
+  echo "is 8 greater than 7:true"
+
+fi
+
+if [ 7 -gt 8 ];
+then 
+  echo"is 7 greater than 8:false"
+  
+fi
+  
+if [ 8 -eq 8 ];
+then 
+  echo "is 8 equals 8:true"
+ 
+fi
+
+if [ 8 -lt 10 ];
+then 
+   echo "is 8 less than 10: true"
+fi
+
+
+
+
