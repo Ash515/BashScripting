d0f3f0ee26d6619bad3800cf39363d83dfb9ff6d
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2022-12-29 13:06:25 +0530

    Added new Decision making.sh file

---
+++ DecisionMaking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "hello" == "hello" ];
+then 
+  echo hello === hello: is true condition
+
+else
+  echo hello == hello: is false condition
+  
+fi
+
+if [ "hello" == "bye" ];
+then
+  echo hello == bye : is true condition
+
+else
+  echo hello == bye : is false condition
+ 
+fi
