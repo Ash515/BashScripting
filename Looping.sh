d6920d5daaadd3a51a69d3f220e762d6733bb3ba
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2022-12-30 13:16:34 +0530

    Added Looping.sh file

---
+++ Looping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+arr=("ashwin","ajith","kishore","sakthi","gowtham")
+for i in "${arr[@]}"
+do 
+echo  $i
+done
+
+
+
+s= "Hi hello this is Ashwin from Zoho corporation"
+for j in $s;
+do
+echo $j
+done
+
+
+
